f1ed8c39aaa9dbf95fd0ba50bd9701b17afee988
Shrithik Raj <lio.srk10@gmail.com>
2025-02-03 13:41:42 +0530

    pracricing whileloop

--- fibonnaci.js
+++ fibonnaci.js
@@ -1,5 +1,4 @@
 
- 
 function fibonacci(n){
    let array1 = [];
    for(let i=0; i<=n; i++){
@@ -15,7 +14,27 @@ function fibonacci(n){
    console.log(array1);
 }
 
+function fibwhileloop(n){
+   let arr2 = [];
+   let i = 0;
+   while(i<=n){
+      if(i===0){
+        arr2[i] = 0;
+      }
+      else if(i===1 || i===2){
+         arr2[i] = 1;
+      }
+      else{
+        arr2[i] = arr2[i-1] + arr2[i-2];
+      }
+      i += 1;
+   }
+   console.log(arr2);
 
-fibonacci(100);
+}
+
+fibwhileloop(10);
+
+// fibonacci(1);
 
 // this problem was to get upto speed with array, loops etc by solving an easy problem.
