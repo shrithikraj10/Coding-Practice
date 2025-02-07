49c590748fbab65436202a7ea3dc64f7fc06f25e
Shrithik Raj <lio.srk10@gmail.com>
2025-02-07 12:46:42 +0530

    Added a practice js file for constructors concept and methods in a constructor

--- JsPractice/ConstructorFunction.js
+++ JsPractice/ConstructorFunction.js
@@ -1,3 +1,5 @@
+const { Console } = require("console");
+
 function Bellboy (name, age, work){
     this.name = name;
     this.age = age;
@@ -5,9 +7,22 @@ function Bellboy (name, age, work){
 }
 // this is an example of a constructor function.
 
+function EmployeDetails(Name, Age, Designation, Tasks){
+    this.Name = Name;
+    this.Age = Age;
+    this.Designation = Designation;
+    this.Tasks = Tasks;
+    this.ToDo = function(){
+        return "My Task is Coding";
+    }
+}
+
 
 var Bellboy1 = new Bellboy("sahil", "24", "SDE");
 var Bellboy2 = new Bellboy("Ronak", "24", "Racer");
 
 
-console.log(Bellboy1, Bellboy2);
+
+var EmployeDetails1 = new EmployeDetails("Jack", 26, "SDE", ["dev","test"]);
+console.log(EmployeDetails1.Name);
+console.log(EmployeDetails1.ToDo());
