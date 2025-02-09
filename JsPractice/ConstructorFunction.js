378996d7126e625230da0311eb6e0551c5344bb4
Shrithik Raj <lio.srk10@gmail.com>
2025-02-09 20:44:34 +0530

    Practicing more function related concepts

--- JsPractice/ConstructorFunction.js
+++ JsPractice/ConstructorFunction.js
@@ -17,6 +17,19 @@ function EmployeDetails(Name, Age, Designation, Tasks){
     }
 }
 
+function add(a,b){
+  return a+b;
+};
+
+function multiply(a,b){
+    return a*b;
+};
+
+function calculator(a,b,operator){
+    return operator(a,b);
+}
+// This is practice of calling functions inside of functions;
+
 
 var Bellboy1 = new Bellboy("sahil", "24", "SDE");
 var Bellboy2 = new Bellboy("Ronak", "24", "Racer");
@@ -26,3 +39,5 @@ var Bellboy2 = new Bellboy("Ronak", "24", "Racer");
 var EmployeDetails1 = new EmployeDetails("Jack", 26, "SDE", ["dev","test"]);
 console.log(EmployeDetails1.Name);
 console.log(EmployeDetails1.ToDo());
+
+console.log(calculator(10,2,multiply));
