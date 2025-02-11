4ee56544cef40b303c941588c6d4d70c18ab5a48
Shrithik Raj <lio.srk10@gmail.com>
2025-02-11 13:12:59 +0530

    The DrumKit website is completed - functionality for kyeboard play added as well as tyding u the code with functions for each action

--- DrumKit/index.js
+++ DrumKit/index.js
@@ -1,39 +1,51 @@
 
-function player(filename){
-    let audio = new Audio(filename);
-    audio.play();
-}
-
 var drums = document.querySelectorAll(".drum").length;
 
 for(i=0; i<drums; i++){
 
    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
        
-       console.log(this.innerHTML);
-       switch(this.innerHTML){
-         case "w":
-            player("sounds/crash.mp3");
-        case "a":
-            player("sounds/kick-bass.mp3");s
-        case "s":
-            player("sounds/snare.mp3");
-        case "d":
-            player("sounds/tom-1.mp3");
-        case "j":
-            player("sounds/tom-2.mp3");
-        case "k":
-            player("sounds/tom-3.mp3");
-        case "l":
-            player("sounds/tom-4.mp3");
+       var letter = this.innerHTML;
+       music(letter);
+} )};
+// listens to the clicks
 
-       }
+function player(filename){
+    let audio = new Audio(filename);
+    audio.play();
+}
+// Gets the required files
 
-   });
+function keyboard(){
+    document.addEventListener("keypress", function(event){
+    music(event.key);
+    }
+)};
+// Listens to the keypresses
 
-}   
+function music(instrument){
+    switch(instrument){
+        case "w":
+           player("sounds/crash.mp3");
+       case "a":
+           player("sounds/kick-bass.mp3");s
+       case "s":
+           player("sounds/snare.mp3");
+       case "d":
+           player("sounds/tom-1.mp3");
+       case "j":
+           player("sounds/tom-2.mp3");
+       case "k":
+           player("sounds/tom-3.mp3");
+       case "l":
+           player("sounds/tom-4.mp3");
+        }
+}
+
+// plays the musics
+
+
+keyboard();
 
 
 
-// var audio = new Audio("sounds/tom-1.mp3");
-//        audio.play();
