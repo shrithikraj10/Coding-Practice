5d38f1632d5b8a125f6afa72d4996d227b6500ec
Shrithik Raj <lio.srk10@gmail.com>
2025-02-16 20:59:07 +0530

    made few changes in jquery practice

--- Jquery/index.js
+++ Jquery/index.js
@@ -1,3 +1,10 @@
 
 $("h1").css("color", "red");
 
+let text = 'Some random text to manipulate';
+
+$("button").click(function(){
+    console.log("event click");
+     $("button").html("<h1>this is the moment</h1>");
+});
+
