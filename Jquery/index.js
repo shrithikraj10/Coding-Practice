6280590f7a6cf66f7c7b80be4676e0fbac8b946a
Shrithik Raj <lio.srk10@gmail.com>
2025-02-17 12:54:58 +0530

    made few changes in jquery practice by adding some html and toggle functionality

--- Jquery/index.js
+++ Jquery/index.js
@@ -1,11 +1,9 @@
 
-$("h1").css("color", "red");
-
 let text = 'Some random text to manipulate';
 
 
 $("button").click(function(){
      
-    $("body").toggleClass('Darkmode');
+    $("body").toggleClass('DarkMode');
 
 })
